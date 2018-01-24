4be55af02075d8a09094342c924770bceea0cc21
jieniz <jieniz@andrew.cmu.edu>
2018-01-24 21:32:39 +1030

    Facial recognition with OpenCV

--- FaceRecognition/src/facerecognition/FaceRecognition.java
+++ FaceRecognition/src/facerecognition/FaceRecognition.java
@@ -27,16 +27,10 @@ import org.bytedeco.javacpp.opencv_core.MatVector;
 
 
 /**
- * I couldn't find any tutorial on how to perform face recognition using OpenCV and Java,
- * so I decided to share a viable solution here. The solution is very inefficient in its
- * current form as the training model is built at each run, however it shows what's needed
- * to make it work.
  *
  * The class below takes two arguments: The path to the directory containing the training
  * faces and the path to the image you want to classify. Note that all images has to be of
- * the same size and that the faces already has to be cropped out of their original images
- * (Take a look here http://fivedots.coe.psu.ac.th/~ad/jg/nui07/index.html if you haven't
- * done the face detection yet).
+ * the same size and that the faces already has to be cropped out of their original images.
  *
  * For the simplicity of this post, the class also requires that the training images have
  * filename format: <label>-rest_of_filename.png. For example:
@@ -46,10 +40,6 @@ import org.bytedeco.javacpp.opencv_core.MatVector;
  * 2-jane_doe_1.png
  * 2-jane_doe_2.png
  * ...and so on.
- *
- * Source: http://pcbje.com/2012/12/doing-face-recognition-with-javacv/
- *
- * @author Petter Christian Bjelland
  */
 public class FaceRecognition {
     public static void main(String[] args) {
